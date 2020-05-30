239471f5b84380994ca4594f165d680d30964234
alby007100 <albericdeanzi1@yahoo.fr>
2020-05-30 13:02:18 -0500

    Update main.py.py

--- PyBank/main.py.py
+++ PyBank/main.py.py
@@ -6,3 +6,7 @@ start_date = datetime.datetime(2017, 2, 1)
 num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
 
 print("Total Month:", num_months)
+jlkfjj;fgibhikjl;bkgih
+kgkfgkljbhkjgikbk'vbk'
+klfgbkk'gk'bk'g\'' \
+             '
